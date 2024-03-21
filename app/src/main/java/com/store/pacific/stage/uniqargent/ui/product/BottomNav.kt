4c7bd38880bb50ca5b51dd24b3671731247f5f8b
sunlei <sunlei20170929@gmail.com>
2024-03-21 20:21:45 +0800

    bottom nav

--- app/src/main/java/com/store/pacific/stage/uniqargent/ui/product/BottomNav.kt
+++ app/src/main/java/com/store/pacific/stage/uniqargent/ui/product/BottomNav.kt
@@ -1,16 +1,6 @@
 package com.store.pacific.stage.uniqargent.ui.product
 
 import android.util.Log
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import com.store.pacific.stage.R
-
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.BottomNavigation
@@ -19,14 +9,21 @@ import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.store.pacific.stage.R
 
 
 sealed class BottomItem(
