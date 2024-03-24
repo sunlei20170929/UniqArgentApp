6985f1172647d36ae3a2c43cd895b6593f1e8bea
sunlei <sunlei20170929@gmail.com>
2024-03-24 21:42:10 +0800

    header and common

--- app/src/main/java/com/store/pacific/stage/networks/commonArgs.kt
+++ app/src/main/java/com/store/pacific/stage/networks/commonArgs.kt
@@ -5,40 +5,8 @@ import okhttp3.RequestBody
 import okhttp3.RequestBody.Companion.toRequestBody
 
 
-data class commonArgs(
-    val appssid:String,
-    val userId:String,
-    val lbs:String = "0,0",
-    val language:String = "es",
-    val versionName:String,
-    val versionCode:String,
-    val deviceId:String,
-    val imei:String,
-    val ip:String,
-    val channel:String = "googleplay",
-    val systemMode:String,
-    val googleMobileNo:String,
-    val googleGaid:String,
-    val googleUserAgent:String
-    )
-fun  generateRequestBody(requestDataMap:Map<String, String>):Map<String, RequestBody> {
-    var requestBodyMap:MutableMap<String, RequestBody> = emptyMap<String, RequestBody>().toMutableMap()
-    for(key in requestDataMap.keys){
-        val requestBody =
-            (requestDataMap[key]?:"").toRequestBody("multipart/form-data".toMediaTypeOrNull())
 
-        requestBodyMap[key] = requestBody
-    }
-    return requestBodyMap;
-}
 
-fun toRequestBody(key:String):RequestBody{
-    return key.toRequestBody("multipart/form-data".toMediaTypeOrNull())
-}
-
-fun strToRequestBody(p: String): RequestBody {
-    return p.toRequestBody("multipart/form-data".toMediaTypeOrNull())
-}
 
 
 
--- app/src/main/java/com/store/pacific/stage/repository/UniqRepository.kt
+++ app/src/main/java/com/store/pacific/stage/repository/UniqRepository.kt
@@ -1,17 +1,15 @@
 package com.store.pacific.stage.repository
 
+import com.store.pacific.stage.CommonParam
+import com.store.pacific.stage.HeaderParam
 import com.store.pacific.stage.networks.BusinessOp
 import com.store.pacific.stage.networks.NetworkService
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.withContext
-import okhttp3.ResponseBody
-import retrofit2.Call
-import retrofit2.http.GET
-import retrofit2.http.HeaderMap
-import retrofit2.http.POST
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -21,15 +19,28 @@ class UniqRepository @Inject constructor(){
 
     private val api: BusinessOp = NetworkService.retrofit.create(BusinessOp::class.java)
 
-    suspend fun getSmscode(num:String): Flow<String> = withContext(Dispatchers.IO) {
-//        val para = toRequestBody(num)
-//        api.getVcode(para)
-        flow {
-            for (i in 1..3) {
-                delay(100)
-                emit(i.toString())
-            }
+    suspend fun getSmscode(header: HeaderParam, common: CommonParam, num:String): Flow<String> = withContext(Dispatchers.IO) {
+        val para = toRequestBody(num)
+        api.getVcode(para)
+    }
+
+    fun  generateRequestBody(requestDataMap:Map<String, String>):Map<String, RequestBody> {
+        var requestBodyMap:MutableMap<String, RequestBody> = emptyMap<String, RequestBody>().toMutableMap()
+        for(key in requestDataMap.keys){
+            val requestBody =
+                (requestDataMap[key]?:"").toRequestBody("multipart/form-data".toMediaTypeOrNull())
+
+            requestBodyMap[key] = requestBody
         }
+        return requestBodyMap;
+    }
+
+    fun toRequestBody(key:String): RequestBody {
+        return key.toRequestBody("multipart/form-data".toMediaTypeOrNull())
+    }
+
+    fun strToRequestBody(p: String): RequestBody {
+        return p.toRequestBody("multipart/form-data".toMediaTypeOrNull())
     }
 
 
